949f3489346feaed3e22b7515a4c75f740798f35
hard_integral <mohammed.a.tazwar04@gmail.com>
2022-06-23 10:17:01 -0400

    Initial push

---
+++ LinC/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace LinC {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, world!");
+        }
+    } 
+}
