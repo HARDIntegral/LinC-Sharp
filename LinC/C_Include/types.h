9ec1ba7bb979ea283b59d3e0c91e32acbaf17d4b
integral <integral@pop-os.localdomain>
2022-07-05 02:59:33 -0400

    Types yay

--- LinC/C_Include/types.h
+++ LinC/C_Include/types.h
@@ -1,6 +1,9 @@
 #ifndef __TYPES_H__
 #define __TYPES_H__
 
-
+#define INTEGER     0x01;   // 00000001
+#define FLOAT       0x02;   // 00000010
+#define DOUBLE      0x04;   // 00000100
+#define BOOL        0x08;   // 00001000
 
 #endif  /* __TYPES_H__ */
--- LinC/Entry.cs
+++ LinC/Entry.cs
@@ -2,6 +2,13 @@ using System;
 using System.Runtime.InteropServices;
 
 namespace LinC {
+    static class Types {
+        public const int INTEGER    = 1;
+        public const int FLOAT      = 2;
+        public const int DOUBLE     = 4;
+        public const int BOOL       = 8;
+    }
+
     class Entry {
         [StructLayout(LayoutKind.Sequential, Pack=1)]
         struct vars_t {
@@ -16,7 +23,7 @@ namespace LinC {
         public static extern unsafe void receive_data(void* data, int type);
 
         // Wrapper methods
-        static unsafe void send<T>(ref T data) where T: unmanaged {
+        static unsafe void send<T>(ref T data, int type) where T: unmanaged {
             fixed (T* data_ref = &data) {
                 receive_data(data_ref, 1);
             };
@@ -27,7 +34,7 @@ namespace LinC {
             Console.WriteLine("Hello, world!");
 
             int test = 5;
-            send(ref test);
+            send(ref test, Types.INTEGER);
         }
     } 
 }
